d19190a2ff9fcd7235739b0a78f54d015abbaeb2
DattasriReddy <rondla.dattasri@gmail.com>
2026-06-08 17:22:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.7 MB (42.68%)

---
+++ 0035-search-insert-position/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int ret=0;
+        for(int i=0;i<nums.size();i++){
+            if(target>nums[i]){
+                ret++;
+            }
+        }
+        return ret;
+    }
+};
