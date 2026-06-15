c8fea3c8c0d8aa61fca667eabf24e70aa14745d8
DattasriReddy <rondla.dattasri@gmail.com>
2026-06-15 17:39:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.2 MB (45.80%)

---
+++ 0190-reverse-bits/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        unsigned int ans=0;
+        unsigned int num=(unsigned int)n;
+        for (int i=0;i<32;i++) {
+            ans=(ans<<1)|(num&1);
+            num>>=1;
+        }
+        return (int)ans;  
+    }
+};
