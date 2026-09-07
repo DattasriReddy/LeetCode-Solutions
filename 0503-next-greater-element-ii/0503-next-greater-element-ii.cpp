adbdc0ec6f7a6c81760f8a0c4dc4e05671734aee
DattasriReddy <rondla.dattasri@gmail.com>
2026-09-07 21:47:25 +0530

    Time: 0 ms (100%), Space: 28.4 MB (83.75%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -6,11 +6,9 @@ public:
         stack<int> st;
         for(int i=0; i<size*2; i++){
             int curr= nums[i % size];
-            while(!st.empty() && curr> nums[st.top()]){
-                int index = st.top();
-                
+            while(!st.empty() && curr> nums[st.top()]){ 
+                ans[st.top()]=curr;
                 st.pop();
-                ans[index]=curr;
             }
             if(i < size) st.push(i);
         }
